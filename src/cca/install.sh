3c688ea7a4d6153590229adffcd522cc56bc9cc5
Eric Van Hensbergen <ericvh@kernel.org>
2023-05-25 12:48:18 +0000

    Reorganize and use config file
    Signed-off-by: Eric Van Hensbergen <ericvh@kernel.org>

--- src/cca/install.sh
+++ src/cca/install.sh
@@ -47,9 +47,16 @@ check_packages() {
 # Ensure apt is in non-interactive to avoid prompts
 export DEBIAN_FRONTEND=noninteractive
 
+# remove previous versions
+rm -rf /usr/local/share/cca
+rm -rf /opt/fvp
+rm -rf /usr/local/bin/lkvm
+rm -rf /usr/local/etc/fvp
+
 # create directories
 mkdir -p /usr/local/share/cca
 mkdir -p /opt/fvp
+mkdir -p /usr/local/etc/fvp
 
 check_packages ca-certificates libatomic1 kmod wget iptables screen telnet
 
@@ -57,6 +64,7 @@ cp lkvm-cca /usr/local/share/cca
 chmod ugo+x /usr/local/share/cca/lkvm-cca
 cp fvp-cca /usr/local/share/cca
 chmod ugo+x /usr/local/share/cca/fvp-cca
+cp cca.config /usr/local/etc/fvp
 
 cd /opt/fvp
 if [ `uname -m` = "aarch64" ] ; then wget https://developer.arm.com/-/media/Files/downloads/ecosystem-models/FVP_Base_RevC-2xAEMvA_11.21_15_Linux64_armv8l.tgz ; \
@@ -77,6 +85,6 @@ cd /usr/local/bin
 wget -O lkvm https://github.com/ericvh/cca-cpu/releases/latest/download/lkvm
 chmod ugo+x /usr/local/bin/lkvm
 
-ln -s /opt/fvp/Base_RevC_AEMvA_pkg/models/Linux64*/FVP_Base_RevC-2xAEMvA fvp
+ln -sf /opt/fvp/Base_RevC_AEMvA_pkg/models/Linux64*/FVP_Base_RevC-2xAEMvA fvp
 
 cd /
